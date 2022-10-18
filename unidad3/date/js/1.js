27cfc60b6f40fbe1a8814cc822360d48365242a8
AJota <alum.aortizg.2021@iesalixar.org>
2022-10-18 19:35:19 +0200

    first commit

---
+++ unidad3/date/js/1.js
@@ -0,0 +1,11 @@
+/**
+ * @returns 
+ */
+function diasfaltan() {
+
+let fecha1 = moment('2022-10-18');
+let fecha2 = moment('2023-06-24');
+
+alert(fecha2.diff(fecha1, 'days'), + ' dias de diferencia');
+
+}
