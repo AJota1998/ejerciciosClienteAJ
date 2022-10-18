e8f855d6232fdb21b76673af5d32c45c61fbe136
AJota <alum.aortizg.2021@iesalixar.org>
2022-10-18 19:47:20 +0200

    tarea

--- unidad3/date/js/1.js
+++ unidad3/date/js/1.js
@@ -1,11 +1,15 @@
 /**
+ * Función que devuelve los días que faltan hasta el fin de curso desde la fecha de hoy. 
  * @returns 
  */
 function diasfaltan() {
 
-let fecha1 = moment('2022-10-18');
-let fecha2 = moment('2023-06-24');
+let hoy = new Date();
+let fincurso = new Date(2023, 06, 24);
 
-alert(fecha2.diff(fecha1, 'days'), + ' dias de diferencia');
+let milisegundos = (fincurso - hoy);
+let dias = (milisegundos /1000 /60 /60 /24) 
+
+alert(parseInt(dias) + ' dias de diferencia');
 
 }
