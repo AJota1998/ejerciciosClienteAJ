ef65e857ca18ff1d154b2ea062025f803f941f70
AJota <alum.aortizg.2021@iesalixar.org>
2022-10-25 19:36:15 +0200

    mas ejjs

---
+++ unidad3/arrays/js/1arrays.js
@@ -0,0 +1,101 @@
+let paises = ['francia', 'inglaterra', 'italia', 'españa', 'alemania', 'belgica', 'holanda', 'portugal']
+
+let opcion = prompt("opciones:\n1 Mostrar número de paises\n2 Mostrar listado de países\n" + 
+"3 Mostrar intervalo de países\n4 Añadir un país\n5 Borrar un país\n6 Consultar un país")
+
+let opcionSecundaria;
+
+if (opcion == 2) {
+    opcionSecundaria = prompt("Introduzca:\n1 orden actual\n2 orden al revés\n3 orden alfabético")
+} else if (opcion == 3) {
+    opcionSecundaria = prompt("Introduzca el intervalo, ejemplo 2-4, 1-3, 2-3")
+}
+
+
+/**
+ * Devuelve el número de elementos del array
+ */
+
+function numElementos() {
+
+}
+
+/**
+ * Devuelve cada elemento del array
+ */
+
+function elementos() {
+
+}
+
+/**
+ * Devuelve los elementos en sentido inverso
+ */
+
+function elementosInverso() {
+
+}
+
+/**
+ * Devuelve el array ordenado alfabéticamente
+ */
+
+function ordenar() {
+
+}
+
+/**
+ * Añade un elemento al principio
+ */
+
+function addPrincipio() {
+
+}
+
+/**
+ * Añade un elemento al final
+ */
+
+function addFinal() {
+
+}
+
+/**
+ * Borra el elemento al principio 
+ */
+
+function deletePrincipio() {
+
+}
+
+/**
+ * Borra el elemento al final
+ */
+
+function deleteFinal() {
+
+}
+
+/**
+ * Muestra el elemento en la posición indicada por el usuario
+ */
+
+function elementoUsuario() {
+
+}
+
+/**
+ * Muestra la posición donde se encuentra el elemento indicado por el usuario
+ */
+
+function numElementoUsuario() {
+
+}
+
+/**
+ * Muestra los elementos comprendidos en el intervalo introducido por el usuario
+ */
+
+function intervaloElementos() {
+
+}
